a8a7ec54cc970c7631f88b1411ff5b2e10879d56
Aulos Plautius <aulospl@gmail.com>
2018-06-08 14:02:37 -0300

    aur packages added

--- Scripts/arch_install/aur_stuff.sh
+++ Scripts/arch_install/aur_stuff.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+# Packages from the Arch User Repository
+
+# fonts
+yaourt -S --noconfirm ttf-meslo
+
+# chat
+yaourt -S --noconfirm discord
+
+# utilities
+yaourt -S --noconfirm inxi
+yaourt -S --noconfirm tty-clock
+
+# vim plugins
+yaourt -S --noconfirm vim-colors-solarized-git
+
